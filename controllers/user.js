6827ee4555d52ffc2ad79a5882ee6c10fde88c5f
Deepanshu Tiwari <ddepu11@gmail.com>
2021-05-24 15:54:57 +0530

    Sign up method created

--- controllers/user.js
+++ controllers/user.js
@@ -4,4 +4,10 @@ const logIn = (req, res) => {
   res.json({ msg: "Log in request", data: req.body });
 };
 
-export { logIn };
+// @desc   Handling User Sign Up
+// @route  POST  /user/sign-up
+const signUp = (req, res) => {
+  res.json({ msg: "Sign up request!!!", data: req.body });
+};
+
+export { logIn, signUp };
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { logIn } from "../controllers/user.js";
+import { logIn, signUp } from "../controllers/user.js";
 
 const router = Router();
 
 // Loggin in user
 router.post("/login", logIn);
+
+router.post("/sign-up", signUp);
 
 export default router;
