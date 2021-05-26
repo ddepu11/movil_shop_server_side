5445ecc508b4b25d6a54262ef4d2c232b5c6f278
Deepanshu Tiwari <ddepu11@gmail.com>
2021-05-26 23:00:50 +0530

    password hashed

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 import User from "../modals/User.js";
+import { genSalt, hash } from "bcrypt";
 
 // @desc   Handling User Log in
 // @route  POST  /user/login
@@ -9,30 +10,34 @@ const logIn = (req, res) => {
 // @desc   Handling User Sign Up
 // @route  POST  /user/sign-up
 const signUp = async (req, res) => {
-  const { email, phoneNumber } = req.body;
+  let { email, phoneNumber, password } = req.body;
 
   try {
-    const userWithThisEmailAlreadyExists = await User.findOne({ email: email });
-    const userWithThisPhoneNumberAlreadyExists = await User.findOne({
+    // Password hashing
+    const salt = await genSalt(10);
+    password = hash(password, salt);
+
+    // Find if email already exists
+    const doesEmailAlreadyExists = await User.findOne({ email: email });
+
+    // Find if email already exists
+    const doesPNAlreadyExists = await User.findOne({
       phoneNumber: phoneNumber,
     });
 
     // When the user enters already existing credentials
-    if (userWithThisEmailAlreadyExists) {
+    if (doesEmailAlreadyExists) {
       res.status(409).json({
         msg: "This Email is already being used by someone else!!!",
       });
-    } else if (userWithThisPhoneNumberAlreadyExists) {
+    } else if (doesPNAlreadyExists) {
       res.status(409).json({
         msg: "This phone number is already being used by someone else!!!",
       });
     }
 
     // When the user enters fresh credentials
-    if (
-      !userWithThisEmailAlreadyExists &&
-      !userWithThisPhoneNumberAlreadyExists
-    ) {
+    if (!doesEmailAlreadyExists && !doesPNAlreadyExists) {
       const user = new User(req.body);
       await user.save();
       res
