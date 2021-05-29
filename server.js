9736a4da7e4b1cbf1b3dd36bb1d1e043669bffdf
Deepanshu Tiwari <ddepu11@gmail.com>
2021-05-29 14:56:31 +0530

    uninstalled eslint

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ import { connectTODB } from "./database/connection.js";
 const PORT = process.env.PORT || 5000;
 const DB_URI = process.env.DB_URI;
 
-
 connectTODB(DB_URI).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}`);
   });
 });
+
+console.log("");
