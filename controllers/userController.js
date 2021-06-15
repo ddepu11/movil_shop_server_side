abadfd7e349ff5855398e69b9bfd989fdd358c64
Deepanshu Tiwari <ddepu11@gmail.com>
2021-06-15 10:18:13 +0530

    role is also beign sent while auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,11 +4,10 @@ import User from '../modals/User.js';
 import generateAuthToken from '../utils/generateAuthToken.js';
 
 const authUser = async (req, res) => {
+  const { firstName, lastName, role } = req.userInfo;
+
   res.status(200).json({
-    user: {
-      firstName: req.userInfo.firstName,
-      lastName: req.userInfo.lastName,
-    },
+    user: { firstName, lastName, role },
   });
 };
 
