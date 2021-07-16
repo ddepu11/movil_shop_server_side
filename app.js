c00ea1b3d84c219cfdb603f5e4af5edcf093b6a5
Deepanshu Tiwari <ddepu11@gmail.com>
2021-07-17 00:46:39 +0530

    fix: remove public from .gitignore

--- app.js
+++ app.js
@@ -54,8 +54,6 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-console.log(join(__dirname, 'public'));
-
 app.use(express.static(join(__dirname, 'public')));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
